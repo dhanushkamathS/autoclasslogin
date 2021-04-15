4789c15d3d32b245ac834c183e8d4f648a526f04
dhanushkamathS <dhanushkamath@gmail.com>
2021-04-15 17:41:25 +0530

    new version

--- zoomlogin.py
+++ zoomlogin.py
@@ -2,21 +2,26 @@ import webbrowser
 import pyautogui
 import time
 
+print('initial signing into zoom')
+
 url = 'https://zoom.us'
 
 # Open URL in a new tab, if a browser window is already open.
 webbrowser.open_new_tab(url)
 
-time.sleep(5)
+time.sleep(8)
 
 pyautogui.press('tab',presses=12)
-time.sleep(2)
+time.sleep(5)
 
 pyautogui.press('enter')
 
-time.sleep(3)
+time.sleep(5)
 
-pyautogui.press('tab',presses=8)
-time.sleep(2)
+for i in range(0,8):
+    pyautogui.press('tab')
+    time.sleep(1)
+
+time.sleep(3)
 
 pyautogui.press('enter')
